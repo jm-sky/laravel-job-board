68e9a28775bd0fa165e07ff9c23ad796db7904ee
Jan Madeyski <jan.madeyski@gmail.com>
2022-07-05 22:13:22 +0200

    helpers vite_assets

--- app/helpers.php
+++ app/helpers.php
@@ -28,7 +28,7 @@ if (! function_exists('vite_assets')) {
 
         return new HtmlString(<<<HTML
             <script type="module" src="/build/{$manifest['resources/js/app.js']['file']}"></script>
-            <link rel="stylesheet" href="/build/{$manifest['resources/js/app.js']['css'][0]}">
+            <link rel="stylesheet" href="/build/{$manifest['resources/css/app.css']['file']}">
         HTML);
     }
 

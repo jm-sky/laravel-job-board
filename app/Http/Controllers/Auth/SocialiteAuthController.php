06a193323f1959b2be60a46afcf1fca18dad6e13
Jan Madeyski <jan.madeyski@gmail.com>
2023-04-15 12:40:01 +0200

    SocialiteAuth fix

--- app/Http/Controllers/Auth/SocialiteAuthController.php
+++ app/Http/Controllers/Auth/SocialiteAuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -25,12 +27,15 @@ class SocialiteAuthController extends Controller
     {
         $githubUser = Socialite::driver($driver)->user();
 
-        $user = User::updateOrCreate([
-            'provider' => $driver,
-            'provider_id' => $githubUser->id,
-        ], [
+        $user = User::where('provider', $driver)->where('provider_id', $githubUser->id)->first();
+        $user = $user ?? User::where('email', $githubUser->email)->first();
+
+        $user = $user ?? User::create([
             'name' => $githubUser->name,
             'email' => $githubUser->email,
+            'password' => Hash::make(Str::random(32)),
+            'provider' => $driver,
+            'provider_id' => $githubUser->id,
             'provider_token' => $githubUser->token,
             'provider_refresh_token' => $githubUser->refreshToken,
         ]);
